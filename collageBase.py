5b4f8af743ca99f34bf8b5fd9bc67f1a5b0ca1ac
ShawnHillJ <shawnhillj@gmail.com>
2018-06-18 19:12:32 -0700

    working on basic overlay method

--- collageBase.py
+++ collageBase.py
@@ -16,31 +16,54 @@ class MosaicBase:
 
     
     
-    def __init__(self, source, imagetuple):
+    def __init__(self, source, imagelist, out_width=100, out_height=100, sam_size=10):
         assert type(source) == str
-        assert type(imagetuple) == tuple
-        assert type(imagetuple[0]) == 
+        assert type(imagelist) == list
+        assert type(imagetuple[0]) == str
 
+        self.debug_float = 0.0
         self.sourceImage = source
         self.scaledSource = None
         self.sampleImagesNamelist = imagelist
         self.sampleImages = None
+        self.sampleScale = sam_size
+        self.outputWidth, self.outputHeight = out_width, out_height
         
         return
 
     def loadImageSource(self, source):
     '''Overrides the current source (root) image for the mosaic.'''
-    
+        self.sourceImage = source
 
     def loadImages(self, imageList):
     '''Loads in a sequence of images into the collage object.'''
-        self.sampleImages = imageList
-        return
+        self.sampleImagesNamelist = imageList
         
-    def generateMosaic(self, outputName)
-    ''' Generates a collage and outputs the image as a file
-        with the name given as an argument.'''
+    def generateOverlayMosaic(self, output)
+    ''' Generates a mosaic using the simple overlay method.
+        Outputs the image as a file with the name given as
+        an argument.
+
+        still in-progress
+    '''
+        offset = self.sampleScale
+        width, height = self.outputWidth, self.outputHeight
+        self.debug_float = 0.0
+        out_image = Image.new("RGB", (width, height))
+        print(len(self.sampleImages))
+        for x in range(width // offset):
+            print(debug_float)
+            for y in range(height // offset):
+                out_image.paste(self.sampleImages[(y + (x * (width//offset))) % len(self.sampleImages)], (x * offset, y * offset))
+                self.debug_float = (y + (x * (width//offset))) / ((width//offset) * (height//offset))
+
+       out_image.convert(mode="RGBa")
+
+       overlay = Image.new("RGBa")
+
+       out_image.convert(mode="RGB")
+       out_image.save(output)
 
         
-    ''' make new image: PIL.Image.new(mode, size, color=0)
+    # make new image: PIL.Image.new(mode, size, color=0)
         
--- imageReader.py
+++ imageReader.py
@@ -1,14 +1,42 @@
 from PIL import Image
+import os
 
 def getAllNamesFromDir(directory):
     '''Returns a tuple of all the filepaths of the files in the directory.'''
-    pass
+    files = []
+    for filename in os.listdir(directory):
+        if filename.endswith(".jpg") or filename.endswith(".png"):
+            files.append(directory + r'\\' + filename)
+    return files
+
+def getImagesFromDirList(filelist, new_width, new_height, num_of_colors):
+    '''Returns an Image list of the filelist, scaled to the new sizes.'''
+    imagelist = []
+    for filename in filelist:
+        imagelist.append(Image.open(filename))
+
+    scaledlist = []
+    for item in imagelist:
+        new_img = item.quantize(num_of_colors)
+        scaledlist.append(new_img.resize((new_width, new_height)))
+    del imagelist
+    return scaledlist
+
+def getModeColor(img):
+    '''Returns the Mode color value of an Image object.'''
+    colorCount = img.getcolors()
+    colorCount.sort()
+    return colorCount[len(colorCount) - 1][1]
 
 
-def getAverageColor(img):
-    '''Returns the average color value of an Image object.'''
-    pass
-    
+#debugging area
+#im = Image.open("test.jpg")
+#im.show()
+'''
+dir_list = getAllNamesFromDir(r'C:\Users\Devastator\Pictures\testing_series')
+print(dir_list, '\n\n')
 
-im = Image.open("test.jpg")
-im.show()
+img_list = getImagesFromDirList(dir_list, 50, 50, 256)
+output = test_combine(img_list, 1000, 1000, 50)
+output.show()
+'''
